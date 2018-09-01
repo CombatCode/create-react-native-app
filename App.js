7ab2e909ae94d6559849c0033ec7d190d39189df
CombatCode <combatcode@gmail.com>
2018-09-01 10:56:31 +0200

    [#1.3]

--- App.js
+++ App.js
@@ -1,23 +1,90 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, Text, TextInput, Button } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      items: []
+    };
+  }
+
+  saveNote = () => {
+    this.setState({
+      items: this.state.items.concat([{title: (new Date()).toString(), key: (new Date()).toString(), content: this.state.value}]),
+      value: ''
+    })
+  }
+
+  clearNotes = () => {
+    this.setState({
+      items: []
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Add new note</Text>
+
+        <TextInput
+          style={styles.input}
+          onChangeText={(value) => this.setState({value})}
+          value={this.state.value}
+        />
+
+        <Button
+          onPress={this.saveNote}
+          title="Save note"
+          color="#841584"
+        />
+
+        <FlatList data={this.state.items} renderItem={this.renderItem} />
+
+        <Button
+          onPress={this.clearNotes}
+          title="Clear list"
+          color="red"
+        />
       </View>
     );
   }
+
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.content}>{item.content}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
+  item: {
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  content: {
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    width: 140,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
